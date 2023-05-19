10e063784ec95f76a447737425f365caa51c9461
sandesh10000 <sandeshgavali01@gmail.com>
2023-05-19 11:42:25 +0530

    newsDonkey  using react class based components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,104 @@
-import './App.css';
-import React, { Component } from 'react'
-import Navbar from "./components/Navbar"
-import News from "./components/News"
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default class App extends Component {
+  pageSize = 5;
+  apiKey=process.env.REACT_APP_NEWS_API;
+  state={
+    progress:0 
+  }
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
+  }
   render() {
     return (
       <div>
         <Router>
+          <LoadingBar
+            color="#f11946"
+            height={3}
+            progress={this.state.progress}
+          />
           <Navbar />
           <Routes>
-            <Route exact path="/" element={<News key="genral"  pageSize={5} country="in" category="general"></News>} />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={5} country="in" category="entertainment"></News>} />
-            <Route exact path="/health" element={<News key="health" pageSize={5} country="in" category="health"></News>} />
-            <Route exact path="/science" element={<News key="science" pageSize={5} country="in" category="science"></News>} />
-            <Route exact path="/technology" element={<News key="technology" pageSize={5} country="in" category="technology"></News>} />
-            <Route exact path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports"></News>} />
+            <Route
+              exact
+              path="/"
+              element={
+                <News apiKey={this.apiKey} setProgress={this.setProgress}
+                  key="genral"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="general"
+                ></News>
+              }
+            />
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News apiKey={this.apiKey} setProgress={this.setProgress}
+                  key="entertainment"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="entertainment"
+                ></News>
+              }
+            />
+            <Route
+              exact
+              path="/health"
+              element={
+                <News apiKey={this.apiKey} setProgress={this.setProgress}
+                  key="health"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="health"
+                ></News>
+              }
+            />
+            <Route
+              exact
+              path="/science"
+              element={
+                <News apiKey={this.apiKey} setProgress={this.setProgress}
+                  key="science"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="science"
+                ></News>
+              }
+            />
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News apiKey={this.apiKey} setProgress={this.setProgress}
+                  key="technology"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="technology"
+                ></News>
+              }
+            />
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News apiKey={this.apiKey} setProgress={this.setProgress}
+                  key="sports"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="sports"
+                ></News>
+              }
+            />
           </Routes>
         </Router>
       </div>
-    )
+    );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react'
-import donkeynewspaper from './donkey_reading_newspaper.png'
+import React, { Component } from "react";
+import donkeynewspaper from "./donkey_reading_newspaper.png";
 export class NewsItem extends Component {
-    render() {
-        let {title,description,imgurl,newsurl,author,date}=this.props;
-        return (
-            <div className='my-3'>
-                <div className="card " >
-                    <img src={imgurl?imgurl:donkeynewspaper} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <p class="card-text"><small className="text-body-secondary">By {author==null?"Trust Me Bro":author} on {date} </small></p>
-                            <a href={newsurl} target="blank" className="btn btn-sm btn-dark">Read More</a>
-                        </div>
-                </div>
+  render() {
+    let { title, description, imgurl, newsurl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card ">
+          <div className="card-body">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                position: "absolute",
+                right: "0",
+              }}
+            >
+              <span className=" badge rounded-pill bg-danger">{source}</span>
             </div>
-        )
-    }
+            <img
+              src={imgurl ? imgurl : donkeynewspaper}
+              className="card-img-top"
+              alt="..."
+            />
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {author == null ? "Trust Me Bro" : author} on{" "}
+                {new Date(date).toGMTtime}{" "}
+              </small>
+            </p>
+            <a href={newsurl} target="blank" className="btn btn-sm btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewsItem
+export default NewsItem;
